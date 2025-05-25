2ee011183e67b8b6d25a29adeb6ddefe15cd4d48
marcophilip <marcophilipagapito@gmail.com>
2025-05-25 15:31:42 -0700

    route tags created for all components within the app.jsx file using Route and Routes from react-router. mailbox use state cariable defined as well.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar.jsx'
 import MailboxList from './components/MailboxList/MailboxList.jsx'
 import MailboxForm from './components/MailboxForm/MailboxForm.jsx'
-import MailbocDetails from './components/MailboxDetails/MailboxDetails.jsx'
+import MailboxDetails from './components/MailboxDetails/MailboxDetails.jsx'
+import { Route, Routes } from 'react-router'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mailboxes, setMailboxes] = useState([]);
 
   return (
-    <h1>Hello world!</h1>
-  )
-}
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+
+        <Route path="/mailboxes" element={<MailboxList />} />
+
+        <Route path="/new-mailbox" element={<MailboxForm />
+} />
+
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />
+} />
+      </Routes>
+    </>
+  );
+};
 
 export default App
